d3c78f356ff759f00302d532780dd4b9a06c5bee
tzukov039 <tzukov039@gmail.com>
2025-04-24 20:59:00 +0300

    Add files via upload
    Initial upload

---
+++ project/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    open: true
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
+});
